edc60d9703c2e47481ccd837dcbfb995278b00be
Matteo Ciocci <matteociocci08@gmail.com>
2024-06-15 08:38:43 +0200

    Added unity project

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public PlayerController controller;
+    public float runSpeed = 40.0f;
+    float horizontalMove = 0.0f;
+    bool jump = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
